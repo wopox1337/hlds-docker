b687e4bffb37a091c9dd3c63568f756694509712
Sergey Shorokhov <wopox1337@ya.ru>
2023-10-12 21:18:30 +0300

    Add gdb

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 docker run --rm -ti \
+    --ulimit core=1073741824 \
     --name my_public_server \
-    -p 27017:27017/udp \
-    -p 27017:27017/tcp \
+    -p 27017:27015/udp \
+    -p 27017:27015/tcp \
     hlds \
     ./hlds_run \
+        -debug \
         -game cstrike \
         -strictportbind \
         -ip 0.0.0.0 \
         -port 27017 \
+        -maxplayers 10 \
         +sv_lan 0 \
-        +map de_dust2 \
-        -maxplayers 10
+        +map de_dust2
