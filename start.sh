f5d60c26e8684faa5ac1e1bea64b9d2df929e2bc
Sergey Shorokhov <wopox1337@ya.ru>
2023-10-13 09:50:05 +0300

    fixes

--- start.sh
+++ start.sh
@@ -3,12 +3,8 @@ docker run --rm -ti \
     --name my_public_server \
     -p 27016:27016/udp \
     hlds \
-    ./hlds_run \
         -debug \
         -game cstrike \
-        -strictportbind \
-        -ip 0.0.0.0 \
         -port 27016 \
         -maxplayers 10 \
-        +sv_lan 0 \
         +map de_dust2
