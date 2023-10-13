e49c5e94c3c965810e68a9c73a72342abeafc17f
Sergey Shorokhov <wopox1337@ya.ru>
2023-10-13 09:18:25 +0300

    change def port to 27016

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 docker run --rm -ti \
     --ulimit core=1073741824 \
     --name my_public_server \
-    -p 27015:27015/udp \
-    -p 27015:27015/tcp \
-    hlds:amxmodx \
+    -p 27016:27016/udp \
+    hlds \
     ./hlds_run \
         -debug \
         -game cstrike \
         -strictportbind \
         -ip 0.0.0.0 \
-        -port 27015 \
+        -port 27016 \
         -maxplayers 10 \
         +sv_lan 0 \
         +map de_dust2
