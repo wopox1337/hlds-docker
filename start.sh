8217367763b71a5e56e6f0a83134b095a93d8ddc
Sergey Shorokhov <wopox1337@ya.ru>
2023-10-13 08:29:32 +0300

    new

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 docker run --rm -ti \
     --ulimit core=1073741824 \
     --name my_public_server \
-    -p 27017:27015/udp \
-    -p 27017:27015/tcp \
-    hlds \
+    -p 27015:27015/udp \
+    -p 27015:27015/tcp \
+    hlds:amxmodx \
     ./hlds_run \
         -debug \
         -game cstrike \
         -strictportbind \
         -ip 0.0.0.0 \
-        -port 27017 \
+        -port 27015 \
         -maxplayers 10 \
         +sv_lan 0 \
         +map de_dust2
